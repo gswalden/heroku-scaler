60ff3411042ef660eb0deb96e1baf8491f2ebc94
Greg Walden <greg.walden@makerbot.com>
2015-07-07 22:26:18 -0400

    leave it how it found it

--- test/index.js
+++ test/index.js
@@ -1,7 +1,30 @@
+var appName = process.env.APP_NAME;
+if (!appName) {
+  console.log('Please set environment variable APP_NAME');
+  return;
+}
+
+var herokuToken = process.env.HEROKU_TOKEN;
+if (!herokuToken) {
+  console.log('Please set environment variable HEROKU_TOKEN');
+  return;
+}
+
 var scaler = require('../');
 var should = require('chai').should();
+var Heroku = require('heroku-client');
+var heroku = new Heroku({ token: herokuToken });
+var state;
 
 describe('Test a live Heroku app', function() {
+  before(function(done) {
+    heroku.get('/apps/' + appName + '/formation', function(err, app) {
+      if (err) should.fail();
+      state = (app[0].quantity === 1) ? 'on' : 'off';
+      done();
+    });
+  });
+
   it('scaler should start an app', function(done) {
     scaler({
       arg: 'on',
@@ -23,10 +46,10 @@ describe('Test a live Heroku app', function() {
       }
     });
   });
-  
+
   after(function(done) {
     scaler({
-      arg: 'on',
+      arg: state,
       cb: function(err, app) {
         if (err) return;
         done();
